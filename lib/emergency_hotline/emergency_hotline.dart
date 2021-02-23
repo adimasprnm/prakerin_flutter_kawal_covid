3e73368eec801d01a10dfa0dc8c96eef1172e252
adimasprnm <adi_179@smkassalaambandung.sch.id>
2021-02-23 23:47:08 +0700

    API FLUTTER(UNDER WORK)

--- lib/emergency_hotline/emergency_hotline.dart
+++ lib/emergency_hotline/emergency_hotline.dart
@@ -14,6 +14,7 @@ class _EmergencyHotlinePageState extends State<EmergencyHotlinePage> {
           title: Text("Emergency Hotline page"),
         ),
         body: Container(
+          padding: EdgeInsets.all(20),
           child: ListView(
             children: <Widget>[
               Container(
@@ -21,18 +22,122 @@ class _EmergencyHotlinePageState extends State<EmergencyHotlinePage> {
                 width: 200,
                 child: Stack(
                   children: <Widget>[
-                    Center(
-                      child: Text(
-                        "CORONAVIRUS",
-                        style: TextStyle(
-                          color: Colors.grey[800],
-                          fontSize: 35,
+                    Container(
+                      height: 100,
+                      child: Center(
+                        child: Text(
+                          "\nCORONAVIRUS HOTLINE \n           INDONESIA",
+                          style: TextStyle(
+                            fontSize: 25,
+                            color: Colors.grey[800],
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.only(top: 20),
+                      height: 200,
+                      child: Center(
+                        child: Text(
+                          "\n\n\nLayanan darurat via telepon yang disediakan oleh \n          Kemkes dan juga Pemprov DKI Jakarta",
                         ),
                       ),
                     ),
                   ],
                 ),
-              )
+              ),
+              Container(
+                alignment: Alignment.center,
+                height: 170,
+                width: 500,
+                child: Card(
+                  child: Center(
+                    child: Stack(
+                      children: <Widget>[
+                        Container(
+                          padding: EdgeInsets.only(top: 15),
+                          alignment: Alignment.topCenter,
+                          child: Image(
+                            image: AssetImage(
+                              'assets/icons/logo_bandung.png',
+                            ),
+                            height: 55,
+                          ),
+                        ),
+                        Container(
+                          padding: EdgeInsets.fromLTRB(0, 50, 0, 0),
+                          alignment: Alignment.center,
+                          child: Text(
+                            "021 5210 411",
+                            style: TextStyle(
+                              color: Colors.deepPurple[900],
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          padding: EdgeInsets.fromLTRB(0, 85, 0, 0),
+                          alignment: Alignment.center,
+                          child: Text(
+                            "\n Kementrian Kesahatan",
+                            style: TextStyle(
+                              color: Colors.blueGrey[600],
+                              fontSize: 11.5,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                alignment: Alignment.center,
+                height: 170,
+                width: 500,
+                child: Card(
+                  child: Center(
+                    child: Stack(
+                      children: <Widget>[
+                        Container(
+                          padding: EdgeInsets.only(top: 15),
+                          alignment: Alignment.topCenter,
+                          child: Image(
+                            image: AssetImage(
+                              'assets/icons/logo_bandung.png',
+                            ),
+                            height: 55,
+                          ),
+                        ),
+                        Container(
+                          padding: EdgeInsets.fromLTRB(0, 50, 0, 0),
+                          alignment: Alignment.center,
+                          child: Text(
+                            "0812-1212-3119",
+                            style: TextStyle(
+                              color: Colors.deepPurple[900],
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          padding: EdgeInsets.fromLTRB(0, 85, 0, 0),
+                          alignment: Alignment.center,
+                          child: Text(
+                            "\n Kementrian Kesahatan",
+                            style: TextStyle(
+                              color: Colors.blueGrey[600],
+                              fontSize: 11.5,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
